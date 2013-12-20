9d22d7c7ff1241fea5b79d166088171207788fb1
Guðrún Sif Hilmarsdóttir <ghilmarsdottir@handpoint.com>
2013-12-20 10:50:23 +0000

    HS-1241: Application call to enable scanner should be on default settings.

--- headstart/NumPadViewController.m
+++ headstart/NumPadViewController.m
@@ -104,7 +104,7 @@ NSMutableString* formatAmountString(NSString* currency, NSString* amountString){
 
 #pragma mark IBAction
 - (IBAction)startScan {
-    [mainController.heftClient enableScanner:TRUE buttonMode:FALSE];
+    [mainController.heftClient enableScanner];
     [mainController showTransactionViewController:eTransactionScanner];
 }
 
