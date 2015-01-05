98b6753a1fe2f0c5fa48c0dd24c1091dde373b41
Handpoint Developer <handpoint@Olafurs-MacBook-Pro.local>
2015-01-05 13:23:39 +0000

    HS-1589: The iOS SDK will now honor the timeout parameters of host send/receive requests received from the card reader (see VersionHistory.txt for details).
    Also, updated the version history with information from issues fixed before the version history was created.

--- heft/MPosOperation.m
+++ heft/MPosOperation.m
@@ -34,6 +34,17 @@ enum eConnectCondition{
 	NSOutputStream* sendStream;
 	NSInputStream* recvStream;
 	NSConditionLock* connectLock;
+    enum eConnectionState {
+        eConnectionClosed,
+        eConnectionConnecting,
+        eConnectionConnected,
+        eConnectionSending,
+        eConnectionSendingComplete,
+        eConnectionReceiving,
+        eConnectionReceivingComplete,
+    } connectionState;
+    vector<UINT8> connectionSendData;
+    vector<UINT8> connectionReceiveData;
 }
 
 - (id)initWithRequest:(RequestCommand*)aRequest connection:(HeftConnection*)aConnection resultsProcessor:(id<IResponseProcessor>)aProcessor sharedSecret:(NSData*)aSharedSecret{
@@ -45,6 +56,9 @@ enum eConnectCondition{
 		processor = aProcessor;
 		sharedSecret = aSharedSecret;
 		connectLock = [[NSConditionLock alloc] initWithCondition:eNoConnectStateCondition];
+        connectionState = eConnectionClosed;
+        connectionSendData.clear();
+        connectionReceiveData.clear();
 	}
 	return self;
 }
@@ -97,99 +111,259 @@ enum eConnectCondition{
 	}
 }
 
+- (void)cleanUpConnection{
+    if(recvStream) {
+        [recvStream close];
+        [recvStream removeFromRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+        [recvStream setDelegate:nil];
+        recvStream = nil;
+    }
+    if(sendStream) {
+        [sendStream close];
+        [sendStream removeFromRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+        [sendStream setDelegate:nil];
+        sendStream = nil;
+    }
+    connectionState = eConnectionClosed;
+}
+
 #pragma mark IHostProcessor
 
 - (void)stream:(NSStream *)aStream handleEvent:(NSStreamEvent)eventCode{
-	[aStream setDelegate:nil];
-	Assert(eventCode == NSStreamEventOpenCompleted || eventCode == NSStreamEventErrorOccurred);
-	[connectLock lock];
-	[connectLock unlockWithCondition:eReadyStateCondition];
+    LOG(@"stream:aStream handleEvent:%lu", (unsigned long)eventCode);
+
+    if(eventCode & NSStreamEventErrorOccurred){
+        // first remove us as the streams event handler so that we don't accidentally get more events
+        [recvStream setDelegate:nil];
+        [sendStream setDelegate:nil];
+        
+        [connectLock lock];
+        connectionState = eConnectionClosed;
+        [connectLock unlockWithCondition:eReadyStateCondition];
+        return;
+    }
+    
+    if(eventCode & NSStreamEventOpenCompleted){
+        if(connectionState == eConnectionConnecting){
+            [connectLock lock];
+            connectionState = eConnectionConnected;
+            [connectLock unlockWithCondition:eReadyStateCondition];
+        }
+    }
+    
+    if(eventCode & NSStreamEventHasBytesAvailable){
+        if(aStream == recvStream){
+            // note: this event will not be generated again until the server sends us more data
+            NSInteger nrecv;
+            vector<UINT8>::size_type old_size = connectionReceiveData.size();
+            NSUInteger stepSize = 65536; // during testing we used a really small value here (i.e. 1)
+
+            do {
+                connectionReceiveData.resize(old_size + stepSize);
+                nrecv = [recvStream read:&connectionReceiveData[old_size] maxLength:stepSize];
+                // it is possible that we didn't read all available data due to our buffer being too small
+                if(nrecv >= 0){
+                    old_size += nrecv;
+                }else{
+                    // first remove us as the streams event handler so that we don't accidentally get more events
+                    [recvStream setDelegate:nil];
+                    [sendStream setDelegate:nil];
+                    
+                    [connectLock lock];
+                    connectionState = eConnectionClosed;
+                    [connectLock unlockWithCondition:eReadyStateCondition];
+                    return;
+                }
+            } while ([recvStream hasBytesAvailable]);
+            
+            // we now have all the data that was pending
+            connectionReceiveData.resize(old_size);
+            
+            // note: for this event we don't touch the connectionState state variable, or the lock, as it will be handled in the NSSteamEventEndEncountered condition below.
+            
+        } // else there are bytes on the sendStream? ... which makes no sense! ... but if so ... then we just ignore this event
+    }
+    
+    if(eventCode & NSStreamEventHasSpaceAvailable){
+        if(aStream == sendStream){
+            // note: this event will not be generated again until we write something to the stream
+            [connectLock lock];
+            if(connectionState == eConnectionSending){
+                NSInteger written;
+
+                if(connectionSendData.size()){
+                    written = [sendStream write:connectionSendData.data() maxLength:connectionSendData.size()];
+                    if(written < connectionSendData.size()){
+                        connectionSendData.erase(connectionSendData.begin(), connectionSendData.begin() + written);
+                        [connectLock unlock];
+                    } else {
+                        if(written == connectionSendData.size()){
+                            connectionSendData.clear();
+                            connectionState = eConnectionSendingComplete;
+                        }else{
+                            // error:
+                            // first remove us as the streams event handler so that we don't accidentally get more events
+                            [recvStream setDelegate:nil];
+                            [sendStream setDelegate:nil];
+                            
+                            connectionState = eConnectionClosed;
+                            return;
+                        }
+                    
+                        // only free the lock when all data is sent or if there is an error
+                        [connectLock unlockWithCondition:eReadyStateCondition];
+                    }
+                } else {
+                    [connectLock unlock];
+                }
+            } else if(connectionState == eConnectionConnected){
+                // we got this event too soon (as in before we have received a send command from the card reader)
+                connectionState = eConnectionSending; // we will inspect for this when we receive the command from the card reader
+                [connectLock unlock];
+            } else {
+                [connectLock unlock];
+            }
+        } // else there is space available on the recvStream ... which we just ignore
+    }
+    
+    if(eventCode & NSStreamEventEndEncountered){
+        // note: it is entirely possible for the server to close the connection prematurely, before it has received anything from us (e.g.  due to some error server site).
+        [connectLock lock];
+        connectionState = eConnectionReceivingComplete;
+        [connectLock unlockWithCondition:eReadyStateCondition];
+    }
 }
 
 - (RequestCommand*)processConnect:(ConnectRequestCommand*)pRequest{
 	LOG_RELEASE(Logger::eFine, _T("State of financial transaction changed: connecting to bureau %s:%d timeout:%d"), pRequest->GetAddr().c_str(), pRequest->GetPort(), pRequest->GetTimeout());
+    
+    int status = EFT_PP_STATUS_CONNECT_ERROR;
+    
+    [connectLock lock];
+    if(connectionState == eConnectionClosed){
+        connectionState = eConnectionConnecting;
+        connectionSendData.clear();
+        connectionReceiveData.clear();
+        [connectLock unlockWithCondition:eNoConnectStateCondition];
+        
+        NSString* host = @(pRequest->GetAddr().c_str());
+        CFReadStreamRef readStream;
+        CFWriteStreamRef writeStream;
+        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, (__bridge CFStringRef)host, pRequest->GetPort(), &readStream, &writeStream);
+        recvStream = (NSInputStream*)CFBridgingRelease(readStream);
+        sendStream = (NSOutputStream*)CFBridgingRelease(writeStream);
 
-	NSString* host = @(pRequest->GetAddr().c_str());
-	CFReadStreamRef readStream;
-	CFWriteStreamRef writeStream;
-	CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, (__bridge CFStringRef)host, pRequest->GetPort(), &readStream, &writeStream);
-	recvStream = (NSInputStream*)CFBridgingRelease(readStream);
-	sendStream = (NSOutputStream*)CFBridgingRelease(writeStream);
+        if(sendStream && recvStream){
+            [recvStream setProperty:NSStreamSocketSecurityLevelNegotiatedSSL forKey:NSStreamSocketSecurityLevelKey];
+            //[sendStream setProperty:NSStreamSocketSecurityLevelNegotiatedSSL forKey:NSStreamSocketSecurityLevelKey];
+            [recvStream setDelegate:self];
+            [sendStream setDelegate:self];
+            [recvStream scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+            [sendStream scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
 
-	[recvStream setProperty:NSStreamSocketSecurityLevelNegotiatedSSL forKey:NSStreamSocketSecurityLevelKey];
-	//[sendStream setProperty:NSStreamSocketSecurityLevelNegotiatedSSL forKey:NSStreamSocketSecurityLevelKey];
-	[recvStream setDelegate:self];
-	NSRunLoop* mainRunLoop = [NSRunLoop mainRunLoop];
-	[recvStream scheduleInRunLoop:mainRunLoop forMode:NSDefaultRunLoopMode];
-	//[sendStream scheduleInRunLoop:mainRunLoop forMode:NSDefaultRunLoopMode];
-	[recvStream open];
-	[sendStream open];
-
-	int status = EFT_PP_STATUS_SUCCESS;
-	if(sendStream && recvStream){
-		if([connectLock lockWhenCondition:eReadyStateCondition beforeDate:[NSDate dateWithTimeIntervalSinceNow:pRequest->GetTimeout()]]){
-			[connectLock unlockWithCondition:eNoConnectStateCondition];
-			if(recvStream.streamStatus == NSStreamStatusOpen){
-				LOG_RELEASE(Logger::eFine, _T("State of financial transaction changed: connected to bureau"));
-			}
-			else{
-				LOG_RELEASE(Logger::eWarning, _T("Error connecting bureau."));
-				status = EFT_PP_STATUS_CONNECT_ERROR;
-			}
-		}
-		else{
-			LOG_RELEASE(Logger::eWarning, _T("Error connecting bureau (timeout)."));
-			status = EFT_PP_STATUS_CONNECT_TIMEOUT;
-		}
-	}
-	else{
-		LOG_RELEASE(Logger::eWarning, _T("Error connecting bureau."));
-		status = EFT_PP_STATUS_CONNECT_ERROR;
-	}
-
-	return new HostResponseCommand(CMD_HOST_CONN_RSP, status);//kCFStreamPropertySSLSettings
+            [recvStream open];
+            [sendStream open];
+            
+            if([connectLock lockWhenCondition:eReadyStateCondition beforeDate:[NSDate dateWithTimeIntervalSinceNow:pRequest->GetTimeout()]]){
+                [connectLock unlockWithCondition:eNoConnectStateCondition];
+                if(recvStream.streamStatus == NSStreamStatusOpen){
+                    LOG_RELEASE(Logger::eFine, _T("State of financial transaction changed: connected to bureau"));
+                    return new HostResponseCommand(CMD_HOST_CONN_RSP, EFT_PP_STATUS_SUCCESS);
+                }else{
+                    LOG_RELEASE(Logger::eWarning, _T("Error connecting bureau."));
+                    status = EFT_PP_STATUS_CONNECT_ERROR;
+                }
+            }else{
+                LOG_RELEASE(Logger::eWarning, _T("Error connecting bureau (timeout)."));
+                status = EFT_PP_STATUS_CONNECT_TIMEOUT;
+            }
+        }else{
+            LOG_RELEASE(Logger::eWarning, _T("Error connecting bureau."));
+            status = EFT_PP_STATUS_CONNECT_ERROR;
+        }
+        
+        // if we get to here then we encountered an error
+        [self cleanUpConnection];
+    }else{
+        [connectLock unlock];
+        // not sure how or what happened on the card reader side ...
+        // ... but we are apparently already serving a server connection from the card reader ?!!!
+        // (not to even mention the question of how this request got here while we are blocked somewhere else)
+        // ... so, instead of trying to be graceful about it we will simply behave like our panties are in a rutt.
+        status = EFT_PP_STATUS_CONNECT_ERROR;
+        
+        // also, note that we won't touch the "current" connection
+    }
+    
+    return new HostResponseCommand(CMD_HOST_CONN_RSP, status);
 }
 
 - (RequestCommand*)processSend:(SendRequestCommand*)pRequest{
 	LOG_RELEASE(Logger::eFine, _T("Request to bureau (length:%d): %@"), pRequest->GetLength(), [[NSString alloc] initWithBytes:pRequest->GetData() length:pRequest->GetLength() encoding:NSUTF8StringEncoding]);
+    
+    if(sendStream) {
+        //connectionState = eConnectionSending;
+        connectionSendData.assign(pRequest->GetData(), pRequest->GetData() + pRequest->GetLength());
+        
+        [connectLock lock];
+        if(connectionState == eConnectionSending){
+            // the stream is already waiting for data from us
+            NSInteger written;
 
-	//while(![sendStream hasSpaceAvailable]);
-	NSInteger nwrite = [sendStream write:pRequest->GetData() maxLength:pRequest->GetLength()];
-	LOG(@"sent to server %d bytes", nwrite);
+            written = [sendStream write:connectionSendData.data() maxLength:connectionSendData.size()];
+            if(written < connectionSendData.size()){
+                connectionSendData.erase(connectionSendData.begin(), connectionSendData.begin() + written);
+                [connectLock unlockWithCondition:eNoConnectStateCondition];
 
-	if(nwrite != pRequest->GetLength()){
-		LOG_RELEASE(Logger::eWarning, _T("Error sending bureau data"));
-		return new HostResponseCommand(CMD_HOST_SEND_RSP, EFT_PP_STATUS_SENDING_ERROR);
-	}
-	return new HostResponseCommand(CMD_HOST_SEND_RSP, EFT_PP_STATUS_SUCCESS);
+                if([connectLock lockWhenCondition:eReadyStateCondition beforeDate:[NSDate dateWithTimeIntervalSinceNow:pRequest->GetTimeout()]]){
+                    if(connectionState == eConnectionSendingComplete){
+                        [connectLock unlockWithCondition:eNoConnectStateCondition];
+                        return new HostResponseCommand(CMD_HOST_SEND_RSP, EFT_PP_STATUS_SUCCESS);
+                    } // else send error
+                } // else send timeout
+            }else{
+                if(written == connectionSendData.size()){
+                    connectionSendData.clear();
+                    connectionState = eConnectionSendingComplete;
+                    [connectLock unlock];
+                    return new HostResponseCommand(CMD_HOST_SEND_RSP, EFT_PP_STATUS_SUCCESS);
+                }
+            }
+        }
+        [connectLock unlock];
+        
+    } // else this was a connection error
+    
+    // if we get to here then we encountered an error
+    [self cleanUpConnection];
+    
+    return new HostResponseCommand(CMD_HOST_SEND_RSP, EFT_PP_STATUS_SENDING_ERROR);
 }
 
 - (RequestCommand*)processReceive:(ReceiveRequestCommand*)pRequest{
 	LOG(_T("Recv :%d bytes, %ds timeout"), pRequest->GetDataLen(), pRequest->GetTimeout());
+    
+    if(recvStream) {
+        if([connectLock lockWhenCondition:eReadyStateCondition beforeDate:[NSDate dateWithTimeIntervalSinceNow:pRequest->GetTimeout()]]){
+            if(connectionState == eConnectionReceivingComplete)
+            {
+                [connectLock unlockWithCondition:eNoConnectStateCondition];
+                LOG_RELEASE(Logger::eFine, _T("Response from bureau (length:%d): "), connectionReceiveData.size());
+                return new ReceiveResponseCommand(connectionReceiveData);
+            } // else receive error
+            
+            [connectLock unlockWithCondition:eNoConnectStateCondition];
+        } // else receive timeout
+    } // else there was a connect or send error
 
-	vector<UINT8> data;
-	int stepSize = 4096;
-	long nrecv = 0;
-	//while([recvStream hasBytesAvailable]){
-	do{
-        vector<UINT8>::size_type old_size = data.size();
-		data.resize(old_size + stepSize);
-		nrecv = [recvStream read:&data[old_size] maxLength:stepSize];
-		if(nrecv < 0)
-			break;
-		data.resize(old_size + nrecv);
-	//}
-	}while(nrecv);
-
-	LOG_RELEASE(Logger::eFine, _T("Response from bureau (length:%d): "), data.size());
-	return data.size() && nrecv >= 0 ? new ReceiveResponseCommand(data) : new HostResponseCommand(CMD_HOST_RECV_RSP, EFT_PP_STATUS_RECEIVING_ERROR);
+    // if we get to here then we encountered an error
+    [self cleanUpConnection];
+    
+    return new HostResponseCommand(CMD_HOST_RECV_RSP, EFT_PP_STATUS_RECEIVING_ERROR);
 }
 
 - (RequestCommand*)processDisconnect:(DisconnectRequestCommand*)pRequest{
-	[recvStream close];
-	[sendStream close];
-	recvStream = nil;
-	sendStream = nil;
+    [self cleanUpConnection];
 	LOG_RELEASE(Logger::eFine, _T("State of financial transaction changed: disconnected"));
 	return new HostResponseCommand(CMD_HOST_DISC_RSP, EFT_PP_STATUS_SUCCESS);
 }
