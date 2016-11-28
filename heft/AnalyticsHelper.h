d6bd9595488fc4109d3eb73880fbbf5a713814b9
Jón Hilmar Gústafsson <jonhilmarg@gmail.com>
2016-11-28 14:31:38 +0000

    Adding production project keys

--- heft/AnalyticsHelper.h
+++ heft/AnalyticsHelper.h
@@ -9,9 +9,16 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+// DEV Project keys
 #define DEV_KEEN_PROJECTID @"56afbb7e46f9a76bfe19bfdc"
 #define DEV_KEEN_WRITEKEY @"6460787402f46a7cafef91ec1d666cc37e14cc0f0bc26a0e3066bfc2e3c772d83a91a99f0ddec23a59fead9051e53bb2e2693201df24bd29eac9c78a61a2208993e9cef175bca6dc029ef28a93a0e5e135201bda7d6a98b2aa1f5aa76c5a4002"
-#define KEEN_MANAGERCREATED @"managerCreated"
+// End DEV Project keys
+
+// Production Project Keys
+#define KEEN_PROJECTID @"56afc865672e6c6e5a9dc431"
+#define KEEN_WRITEKEY @"68acf442839a15c214424f06d8b3298c2b0a9901e0cf3068977ad13d24b8e8e3590c853f2935772d632aefaef5c60b4f35383bd01fd8d65f9bf37a57f3ba2e6de21317e9f91ba1172ca79040e237e354b3f71c2147e3cca2250fb263a49d5a09"
+// End Production Project Keys
+
 #define KEEN_MANAGERACTION @"managerAction"
 #define KEEN_CARDREADERACTION @"cardreaderAction"
 
